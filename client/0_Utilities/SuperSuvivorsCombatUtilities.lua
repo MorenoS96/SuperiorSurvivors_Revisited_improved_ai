7a802368eef5efc63789033d970a728dd41d3907
moreno <morenos1610@gmail.com>
2025-09-15 00:33:21 +0200

    test 1

--- client/0_Utilities/SuperSuvivorsCombatUtilities.lua
+++ client/0_Utilities/SuperSuvivorsCombatUtilities.lua
@@ -1,145 +1,190 @@
 -- this file has methods related to npc combat
-
-SuperSurvivorsAmmoBoxes = {   -- for the loot stores that are spawned with preset spawns.
-  "Base.223Box",
-  "Base.308Box",
-  "Base.Bullets9mm",
-  "Base.Bullets9mm",
-  "Base.Bullets9mm",
-  "Base.ShotgunShellsBox",
-  "Base.ShotgunShellsBox",
-  "Base.ShotgunShellsBox",
-}
-
+SuperSurvivorsAmmoBoxes = { -- for the loot stores that are spawned with preset spawns.
+"Base.223Box", "Base.308Box", "Base.Bullets9mm", "Base.Bullets9mm", "Base.Bullets9mm", "Base.ShotgunShellsBox",
+"Base.ShotgunShellsBox", "Base.ShotgunShellsBox"}
 
 --- Gets a ammo box of an ammo typ
 ---@param bullets string any ammo type
 ---@return string returns the ammo box name
 function getAmmoBox(bullets)
-	if(isModEnabled("ORGM")) then return bullets.."_Box" end
+    if (isModEnabled("ORGM")) then
+        return bullets .. "_Box"
+    end
 
-	if (bullets == "BB177") then return "BB177Box"
-	elseif (bullets == "Bullets22") then return "Bullets22Box"
-	elseif (bullets == "Bullets57") then return "Bullets57Box"
-	elseif (bullets == "Bullets380") then return "Bullets380Box"
-	elseif (bullets == "Bullets9mm") then return "Bullets9mmBox"
-	elseif (bullets == "Bullets38") then return "Bullets38Box"
-	elseif (bullets == "Bullets357") then return "Bullets357Box"
-	elseif (bullets == "Bullets45") then return "Bullets45Box"
-	elseif (bullets == "Bullets45LC") then return "Bullets45LCBox"
-	elseif (bullets == "Bullets44") then return "Bullets44Box"
-	elseif (bullets == "Bullets4570") then return "Bullets4570Box"
+    if (bullets == "BB177") then
+        return "BB177Box"
+    elseif (bullets == "Bullets22") then
+        return "Bullets22Box"
+    elseif (bullets == "Bullets57") then
+        return "Bullets57Box"
+    elseif (bullets == "Bullets380") then
+        return "Bullets380Box"
+    elseif (bullets == "Bullets9mm") then
+        return "Bullets9mmBox"
+    elseif (bullets == "Bullets38") then
+        return "Bullets38Box"
+    elseif (bullets == "Bullets357") then
+        return "Bullets357Box"
+    elseif (bullets == "Bullets45") then
+        return "Bullets45Box"
+    elseif (bullets == "Bullets45LC") then
+        return "Bullets45LCBox"
+    elseif (bullets == "Bullets44") then
+        return "Bullets44Box"
+    elseif (bullets == "Bullets4570") then
+        return "Bullets4570Box"
 
-	elseif (bullets == "410gShotgunShells")then return "410gShotgunShellsBox"
-	elseif (bullets == "20gShotgunShells") then return "20gShotgunShellsBox"
-	elseif (bullets == "ShotgunShells") then return "ShotgunShellsBox"
-	elseif (bullets == "10gShotgunShells") then return "10gShotgunShellsBox"
-	elseif (bullets == "4gShotgunShells") then return "4gShotgunShellsBox"
+    elseif (bullets == "410gShotgunShells") then
+        return "410gShotgunShellsBox"
+    elseif (bullets == "20gShotgunShells") then
+        return "20gShotgunShellsBox"
+    elseif (bullets == "ShotgunShells") then
+        return "ShotgunShellsBox"
+    elseif (bullets == "10gShotgunShells") then
+        return "10gShotgunShellsBox"
+    elseif (bullets == "4gShotgunShells") then
+        return "4gShotgunShellsBox"
 
-	elseif (bullets == "223Bullets") then return "223Box"
-	elseif (bullets == "556Bullets") then return "556Box"
-	elseif (bullets == "762x39Bullets") then return "762x39Box"
-	elseif (bullets == "308Bullets") then return "308Box"
-	elseif (bullets == "762x51Bullets") then return "762x51Box"
-	elseif (bullets == "762x54rBullets") then return "762x54rBox"
-	elseif (bullets == "3006Bullets") then return "3006Box"
-	elseif (bullets == "50BMGBullets") then return "50BMGBox"
+    elseif (bullets == "223Bullets") then
+        return "223Box"
+    elseif (bullets == "556Bullets") then
+        return "556Box"
+    elseif (bullets == "762x39Bullets") then
+        return "762x39Box"
+    elseif (bullets == "308Bullets") then
+        return "308Box"
+    elseif (bullets == "762x51Bullets") then
+        return "762x51Box"
+    elseif (bullets == "762x54rBullets") then
+        return "762x54rBox"
+    elseif (bullets == "3006Bullets") then
+        return "3006Box"
+    elseif (bullets == "50BMGBullets") then
+        return "50BMGBox"
 
-	elseif (bullets == "Nails") then return "NailsBox" -- For Nailgun Mod
-	end
+    elseif (bullets == "Nails") then
+        return "NailsBox" -- For Nailgun Mod
+    end
 
-	return ""
+    return ""
 end
 
 --- func desc
 ---@param box string ammo box name
 ---@return integer returns the amount of bullets inside of the ammo box
 function getBoxCount(box)
-	if (box == "BB177Box") then return 500
-	elseif (box == "Bullets22Box") then return 100
-	elseif (box == "Bullets57Box") then return 50
-	elseif (box == "Bullets380Box") then return 50
-	elseif (box == "Bullets9mmBox") then return 50
-	elseif (box == "Bullets38Box") then return 50
-	elseif (box == "Bullets357Box") then return 50
-	elseif (box == "Bullets45Box") then return 50
-	elseif (box == "Bullets45LCBox") then return 50
-	elseif (box == "Bullets44Box") then return 50
-	elseif (box == "Bullets4570Box") then return 20
+    if (box == "BB177Box") then
+        return 500
+    elseif (box == "Bullets22Box") then
+        return 100
+    elseif (box == "Bullets57Box") then
+        return 50
+    elseif (box == "Bullets380Box") then
+        return 50
+    elseif (box == "Bullets9mmBox") then
+        return 50
+    elseif (box == "Bullets38Box") then
+        return 50
+    elseif (box == "Bullets357Box") then
+        return 50
+    elseif (box == "Bullets45Box") then
+        return 50
+    elseif (box == "Bullets45LCBox") then
+        return 50
+    elseif (box == "Bullets44Box") then
+        return 50
+    elseif (box == "Bullets4570Box") then
+        return 20
 
-	elseif (box == "410gShotgunShellsBox") then return 25
-	elseif (box == "20gShotgunShellsBox") then return 25
-	elseif (box == "ShotgunShellsBox") then return 25
-	elseif (box == "10gShotgunShellsBox") then return 25
-	elseif (box == "4gShotgunShellsBox") then return 10
+    elseif (box == "410gShotgunShellsBox") then
+        return 25
+    elseif (box == "20gShotgunShellsBox") then
+        return 25
+    elseif (box == "ShotgunShellsBox") then
+        return 25
+    elseif (box == "10gShotgunShellsBox") then
+        return 25
+    elseif (box == "4gShotgunShellsBox") then
+        return 10
 
-	elseif (box == "223Box") then return 20
-	elseif (box == "556Box") then return 20
-	elseif (box == "762x39Box") then return 20
-	elseif (box == "308Box") then return 20
-	elseif (box == "762x51Box") then return 20
-	elseif (box == "762x54rBox") then return 20
-	elseif (box == "3006Box") then return 20
-	elseif (box == "50BMGBox") then return 10
-	elseif (box == "NailsBox") then return 100 -- For Nailgun Mod
+    elseif (box == "223Box") then
+        return 20
+    elseif (box == "556Box") then
+        return 20
+    elseif (box == "762x39Box") then
+        return 20
+    elseif (box == "308Box") then
+        return 20
+    elseif (box == "762x51Box") then
+        return 20
+    elseif (box == "762x54rBox") then
+        return 20
+    elseif (box == "3006Box") then
+        return 20
+    elseif (box == "50BMGBox") then
+        return 10
+    elseif (box == "NailsBox") then
+        return 100 -- For Nailgun Mod
 
-	elseif (isModEnabled("ORGM")) then return 50
+    elseif (isModEnabled("ORGM")) then
+        return 50
 
-	else return 0
-	end
+    else
+        return 0
+    end
 end
 
 function SurvivorTogglePVP()
 
-	if(IsoPlayer.getCoopPVP() == true) then
-		getSpecificPlayer(0):Say("PVP Disabled");
-		IsoPlayer.setCoopPVP(false);
-		getSpecificPlayer(0):getModData().PVP = false;
-		PVPDefault = false;
-		PVPButton:setImage(PVPTextureOff)
-	elseif(IsoPlayer.getCoopPVP() == false) then
+    if (IsoPlayer.getCoopPVP() == true) then
+        getSpecificPlayer(0):Say("PVP Disabled");
+        IsoPlayer.setCoopPVP(false);
+        getSpecificPlayer(0):getModData().PVP = false;
+        PVPDefault = false;
+        PVPButton:setImage(PVPTextureOff)
+    elseif (IsoPlayer.getCoopPVP() == false) then
 
-		IsoPlayer.setCoopPVP(true);
-		if(ForcePVPOn ~= true) then
-			getSpecificPlayer(0):getModData().PVP = true;
-			PVPDefault = true;
-			getSpecificPlayer(0):Say("PVP Enabled");
-		else
-			getSpecificPlayer(0):Say("PVP Forced On");
-		end
-		ForcePVPOn = false;
-		PVPButton:setImage(PVPTextureOn)
-	end
+        IsoPlayer.setCoopPVP(true);
+        if (ForcePVPOn ~= true) then
+            getSpecificPlayer(0):getModData().PVP = true;
+            PVPDefault = true;
+            getSpecificPlayer(0):Say("PVP Enabled");
+        else
+            getSpecificPlayer(0):Say("PVP Forced On");
+        end
+        ForcePVPOn = false;
+        PVPButton:setImage(PVPTextureOn)
+    end
 end
 
 ---	gets in the database the ammo type for the weapon 'weapon' 
 ---@param weapon any weapon to have the ammo searched
 ---@param incModule any (not being used)
 ---@return any returns the ammo type of the gun or nil if not found
-function getAmmoType(weapon,incModule)
+function getAmmoType(weapon, incModule)
 
-	if(weapon == nil) or (weapon:getAmmoType() == nil) then 
-    return nil 
-  end
+    if (weapon == nil) or (weapon:getAmmoType() == nil) then
+        return nil
+    end
 
-	local modulename ='Base';
-	local wepType = weapon:getType();
-	local out = weapon:getAmmoType()
+    local modulename = 'Base';
+    local wepType = weapon:getType();
+    local out = weapon:getAmmoType()
 
-  -- search for a magazine
-	if(out == nil) then
-		local s = weapon:getMagazineType();
-		i, j = string.find(s, "Clip")
-		out = s:sub( i )
-	end
+    -- search for a magazine
+    if (out == nil) then
+        local s = weapon:getMagazineType();
+        i, j = string.find(s, "Clip")
+        out = s:sub(i)
+    end
 
-	if(out == nil) then
-		print("no bullets found for weapon: " .. wepType)
-		return nil
-	end
+    if (out == nil) then
+        print("no bullets found for weapon: " .. wepType)
+        return nil
+    end
 
-	out = out:sub( 6 )
-	--[[
+    out = out:sub(6)
+    --[[
 	print("weapong type: "..wepType);
 	local wepdata = ReloadUtil:getWeaponData(wepType);
 	if(not wepdata or not wepdata.ammoType) then
@@ -168,51 +213,168 @@ function getAmmoType(weapon,incModule)
 	end
 	--]]
 
-
-	--if(incModule) then out = modulename .. "." .. out; end
-	return out;
+    -- if(incModule) then out = modulename .. "." .. out; end
+    return out;
 
 end
 
 --- gets in the database bullets for the weapon 'weapon' 
 ---@param weapon any a HandWeapon
 ---@param incModule any
-function getAmmoBullets(weapon,incModule)
+function getAmmoBullets(weapon, incModule)
 
-	if(weapon == nil) then 
-    return nil 
-  end
+    if (weapon == nil) then
+        return nil
+    end
 
-	if (instanceof(weapon,"HandWeapon")) and (weapon:isAimedFirearm()) then
-		local bullets = {}
+    if (instanceof(weapon, "HandWeapon")) and (weapon:isAimedFirearm()) then
+        local bullets = {}
 
-		if(isModEnabled("ORGM")) then
-		  local ammoTbl = ORGM.AlternateAmmoTable[getAmmoType(weapon,false)]
-		  if (ammoTbl) then
-		   --for _, name in ipairs(ammoTbl) do
-		    for k=1, #ammoTbl do
-          local name = ammoTbl[k]
+        if (isModEnabled("ORGM")) then
+            local ammoTbl = ORGM.AlternateAmmoTable[getAmmoType(weapon, false)]
+            if (ammoTbl) then
+                -- for _, name in ipairs(ammoTbl) do
+                for k = 1, #ammoTbl do
+                    local name = ammoTbl[k]
 
-          if(incModule) then 
-            table.insert(bullets,"ORGM."..name)
-          else 
-            table.insert(bullets,name) 
-          end
+                    if (incModule) then
+                        table.insert(bullets, "ORGM." .. name)
+                    else
+                        table.insert(bullets, name)
+                    end
 
-		    end
-		  end
+                end
+            end
 
-		  return bullets
-		end
+            return bullets
+        end
 
-		if(incModule) then
-			table.insert(bullets,getAmmoType(weapon,incModule))
-		else
-			table.insert(bullets,getAmmoType(weapon,incModule))
-		end
+        if (incModule) then
+            table.insert(bullets, getAmmoType(weapon, incModule))
+        else
+            table.insert(bullets, getAmmoType(weapon, incModule))
+        end
 
-		return bullets;
-	end
+        return bullets;
+    end
 
-	return nil
+    return nil
+end
+
+-- cache table on NPC
+-- npc._vehicleShootCache = {
+--   vehicle = <IsoVehicle>,
+--   seatIndex = <number>,
+--   windowPart = <VehiclePart>
+-- }
+-- refresh the cache when needed (enter/exit vehicle, seat change, etc.)
+local function RefreshVehicleShootCache(npc)
+    if not npc or not npc.Get then
+        return
+    end
+    local character = npc:Get()
+    if not character then
+        return
+    end
+
+    local vehicle = character:getVehicle()
+    if not vehicle then
+        npc._vehicleShootCache = nil
+        return
+    end
+
+    -- determine current passenger index
+    local passengerIndex = nil
+    if vehicle.passengers then
+        for i = 1, #vehicle.passengers do
+            local p = vehicle.passengers[i]
+            if p and p.character == character then
+                passengerIndex = i - 1
+                break
+            end
+        end
+    end
+    if not passengerIndex then
+        npc._vehicleShootCache = nil
+        return
+    end
+
+    -- find the window part associated with that seat
+    local foundWindow = nil
+    if vehicle.parts then
+        for i = 1, #vehicle.parts do
+            local part = vehicle.parts[i]
+            if part and part.getContainerSeatNumber and part:getContainerSeatNumber() == passengerIndex then
+                if part.getWindow then
+                    local vw = part:getWindow()
+                    if vw then
+                        foundWindow = part
+                        break
+                    end
+                end
+                if part.getChildWindow then
+                    local childWin = part:getChildWindow()
+                    if childWin then
+                        foundWindow = part
+                        break
+                    end
+                end
+            end
+        end
+    end
+
+    npc._vehicleShootCache = {
+        vehicle = vehicle,
+        seatIndex = passengerIndex,
+        windowPart = foundWindow
+    }
+end
+
+-- lightweight check during update
+function CanShootFromVehicle(npc)
+    if not npc or not npc.Get then
+        return false
+    end
+    local character = npc:Get()
+    if not character then
+        return false
+    end
+
+    local cache = npc._vehicleShootCache
+    local vehicle = character:getVehicle()
+
+    -- refresh cache if missing or stale
+    if (not cache) or (cache.vehicle ~= vehicle) then
+        RefreshVehicleShootCache(npc)
+        cache = npc._vehicleShootCache
+    end
+
+    if not cache or not cache.vehicle then
+        return false
+    end
+    if not npc:hasGun() then
+        return false
+    end
+    if not npc.LastEnemeySeen then
+        return false
+    end
+    if not npc:isEnemyInRange(npc.LastEnemeySeen) then
+        return false
+    end
+
+    -- check window state
+    if cache.windowPart then
+        local vw = cache.windowPart:getWindow()
+        if vw and (vw:isDestroyed() or vw:isOpen()) then
+            return true
+        end
+        if cache.windowPart.getChildWindow then
+            local cw = cache.windowPart:getChildWindow()
+            if cw and (cw:isDestroyed() or cw:isOpen()) then
+                return true
+            end
+        end
+    end
+
+    return false
 end
--- client/6_Tasks/AttackTask.lua
+++ client/6_Tasks/AttackTask.lua
@@ -3,110 +3,108 @@ AttackTask.__index = AttackTask
 
 function AttackTask:new(superSurvivor)
 
-	local o = {}
-	setmetatable(o, self)
-	self.__index = self
-		
-	o.parent = superSurvivor
-	o.Name = "Attack"
-	o.OnGoing = false
-	--o.parent:Speak("starting attack")
-	o.parent:DebugSay(tostring(o.parent:getCurrentTask()).." Started!" )
-	
-	return o
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
+
+    o.parent = superSurvivor
+    o.Name = "Attack"
+    o.OnGoing = false
+    -- o.parent:Speak("starting attack")
+    o.parent:DebugSay(tostring(o.parent:getCurrentTask()) .. " Started!")
+
+    return o
 
 end
 
 function AttackTask:isComplete()
-	local theDistance = getDistanceBetween(self.Target, self.parent.player)
+    local theDistance = getDistanceBetween(self.Target, self.parent.player)
 
-	--self.parent.player:Say( tostring(self.parent:needToFollow()) ..",".. tostring(self.parent:getDangerSeenCount() > 0) ..",".. tostring(self.parent.LastEnemeySeen) ..",".. tostring(not self.parent.LastEnemeySeen:isDead()) ..",".. tostring(self.parent:HasInjury() == false) )
-	if(not self.parent:needToFollow()) and ((self.parent:getDangerSeenCount() > 0) or (self.parent:isEnemyInRange(self.parent.LastEnemeySeen) and self.parent:hasWeapon())) and (self.parent.LastEnemeySeen) and not self.parent.LastEnemeySeen:isDead() and (self.parent:HasInjury() == false) then 
-		return false
-	else
-		if theDistance < 1 then
-			self.parent:StopWalk()
-		end
-		return true 
-	end
+    -- self.parent.player:Say( tostring(self.parent:needToFollow()) ..",".. tostring(self.parent:getDangerSeenCount() > 0) ..",".. tostring(self.parent.LastEnemeySeen) ..",".. tostring(not self.parent.LastEnemeySeen:isDead()) ..",".. tostring(self.parent:HasInjury() == false) )
+    if (not self.parent:needToFollow()) and ((self.parent:getDangerSeenCount() > 0) or
+        (self.parent:isEnemyInRange(self.parent.LastEnemeySeen) and self.parent:hasWeapon())) and
+        (self.parent.LastEnemeySeen) and not self.parent.LastEnemeySeen:isDead() and (self.parent:HasInjury() == false) then
+        return false
+    else
+        if theDistance < 1 then
+            self.parent:StopWalk()
+        end
+        return true
+    end
 end
 
 function AttackTask:isValid()
-	if (not self.parent) or (not self.parent.LastEnemeySeen) or (not self.parent:isInSameRoom(self.parent.LastEnemeySeen)) or (self.parent.LastEnemeySeen:isDead()) then 
-			return false 
-		else 
-			return true 
-	end
+    if (not self.parent) or (not self.parent.LastEnemeySeen) or
+        (not self.parent:isInSameRoom(self.parent.LastEnemeySeen)) or (self.parent.LastEnemeySeen:isDead()) then
+        return false
+    else
+        return true
+    end
 end
 
 function AttackTask:update()
-	if(not self:isValid()) or (self:isComplete()) then return false end
-	
-	if(self.parent:isWalkingPermitted()) then
-		self.parent:NPC_MovementManagement() 		-- For melee movement management
+    if not self:isValid() or self:isComplete() then
+        return false
+    end
 
-		-- Controls the Range of how far / close the NPC should be
-		if self.parent:hasGun() then 					-- Despite the name, it means 'has gun in the npc's hand'
-			if (self.parent:needToReadyGun(weapon)) then
-				self.parent:ReadyGun(weapon)
-			else
-				self.parent:NPC_MovementManagement_Guns() 	-- To move around, it checks for in attack range too
-			end
-		end
-	end
-		
+    local weapon = self.parent.player:getPrimaryHandItem()
+    local theDistance = getDistanceBetween(self.parent.LastEnemeySeen, self.parent.player)
+    local minrange = self.parent:getMinWeaponRange()
+    local NPC_AttackRange = self.parent:isEnemyInRange(self.parent.LastEnemeySeen)
 
-	local theDistance = getDistanceBetween(self.parent.LastEnemeySeen, self.parent.player)
-	local minrange = self.parent:getMinWeaponRange()
-	local NPC_AttackRange = self.parent:isEnemyInRange(self.parent.LastEnemeySeen)
-	
-	-- Controls if the NPC is litreally running or walking state.
-	self.parent:NPC_ShouldRunOrWalk()
+    -- Decide whether NPC should run or walk
+    self.parent:NPC_ShouldRunOrWalk()
 
---	if (NPC_AttackRange) or (theDistance <= minrange) or (theDistance < 0.65) then
-	if (NPC_AttackRange) or (theDistance < 0.65) then
-			--print(self.parent:getName().. " int attack range !" )
-			local weapon = self.parent.player:getPrimaryHandItem()
-			if(not weapon or (not self.parent:usingGun()) or ISReloadWeaponAction.canShoot(weapon))  then
-				--print(self.parent:getName().. " can shoot/attack " )
+    -- Movement management (only if walking is permitted)
+    if self.parent:isWalkingPermitted() then
+        self.parent:NPC_MovementManagement() -- Melee/positioning
+        if self.parent:hasGun() then
+            if self.parent:needToReadyGun(weapon) then
+                self.parent:ReadyGun(weapon)
+            else
+                self.parent:NPC_MovementManagement_Guns() -- Move around for shooting
+            end
+        end
+        self.parent:NPC_ManageLockedDoors()
+    end
 
-				
-				if (self.parent:hasGun()) then -- Gun related conditions
-					if (self.parent:needToReadyGun(weapon)) then
-						self.parent:ReadyGun(weapon)
-					else
-						if (self.parent:Is_AtkTicksZero()) then
-							self.parent:Attack(self.parent.LastEnemeySeen) 
-						else
-							self.parent:AtkTicks_Countdown()
-						end
-					end
-					
-				else -- Melee related conditions
-					if (self.parent:Is_AtkTicksZero()) then
-						self.parent:NPC_Attack(self.parent.LastEnemeySeen)
-					else
-						self.parent:AtkTicks_Countdown()
-					end
-				end	
-				
-			elseif(self.parent:usingGun()) then
-				if(self.parent:ReadyGun(weapon) == false) then self.parent:reEquipMele() end
-				--print(self.parent:getName().. " trying to ready gun" )
-				--self.parent:Wait(1)
-			end	
-			--if(self.parent:usingGun()) then self.parent.Reducer = 0 end -- force delay when using gun
-		
-	elseif(self.parent:isWalkingPermitted()) then
-	
-		self.parent:NPC_ManageLockedDoors() -- To prevent getting stuck in doors
-	--	self.parent:NPC_MovementManagement() -- To move around 
+    -- Decide if NPC is in attack range
+    if NPC_AttackRange or theDistance < 0.65 then
+        -- VEHICLE SHOOTING
+        if self.parent:InVehicle() and self.parent:hasGun() and CanShootFromVehicle(self.parent) then
+            if self.parent:Is_AtkTicksZero() then
+                self.parent:VehicleAttack(self.parent.LastEnemeySeen)
+            else
+                self.parent:AtkTicks_Countdown()
+            end
 
+            -- ON-FOOT GUN ATTACK
+        elseif self.parent:hasGun() then
+            if self.parent:needToReadyGun(weapon) then
+                self.parent:ReadyGun(weapon)
+            else
+                if self.parent:Is_AtkTicksZero() then
+                    self.parent:Attack(self.parent.LastEnemeySeen)
+                else
+                    self.parent:AtkTicks_Countdown()
+                end
+            end
 
-	--	self.parent:DebugSay("walking close to attack:"..tostring(theDistance))
-	else
-		self.parent:DebugSay("ATTACK TASK - something is wrong")
-	end
-	return true
-	
+            -- MELEE ATTACK
+        else
+            if self.parent:Is_AtkTicksZero() then
+                self.parent:NPC_Attack(self.parent.LastEnemeySeen)
+            else
+                self.parent:AtkTicks_Countdown()
+            end
+        end
+
+    elseif self.parent:isWalkingPermitted() then
+        -- Keep moving closer if not in range
+        self.parent:NPC_MovementManagement()
+    else
+        self.parent:DebugSay("ATTACK TASK - something is wrong")
+    end
+
+    return true
 end
